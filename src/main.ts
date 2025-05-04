85e5565877cabb811d2f2257bdcf000b896c8072
Francisco <github.backstage538@passmail.net>
2025-05-04 09:50:26 +0200

    Draw grid

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,46 @@
-import './style.css'
+import { CANVAS_SIZE, CELL_SIZE, GRID_SIZE } from '@/constants';
+import '@/style.css';
+
+class Game {
+  public canvas: HTMLCanvasElement;
+  public ctx: CanvasRenderingContext2D;
+
+  constructor() {
+    this.canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext('2d')!;
+
+    this.canvas.width = CANVAS_SIZE;
+    this.canvas.height = CANVAS_SIZE;
+
+    this.drawGrid();
+  }
+
+  private drawGrid(): void {
+    this.ctx.fillStyle = '#2c3e50';
+    this.ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+    this.ctx.strokeStyle = '#ffffff33';
+    this.ctx.lineWidth = 1;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(0, CANVAS_SIZE);
+    this.ctx.stroke();
+
+    for (let i = 0; i < GRID_SIZE; i++) {
+      // Vertical lines
+      this.ctx.beginPath();
+      this.ctx.moveTo(CELL_SIZE * i, 0);
+      this.ctx.lineTo(CELL_SIZE * i, CANVAS_SIZE);
+      this.ctx.stroke();
+
+      // Horizontal lines
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, CELL_SIZE * i);
+      this.ctx.lineTo(CANVAS_SIZE, CELL_SIZE * i);
+      this.ctx.stroke();
+    }
+  }
+}
+
+window.onload = () => new Game();
